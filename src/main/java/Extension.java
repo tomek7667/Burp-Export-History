84f51582d1ad189922661dc01b973eea0f1f949d
tomek7667 <git@cyber-man.pl>
2026-04-09 23:28:52 +0200

    .idea stuff; +test panel

--- src/main/java/Extension.java
+++ src/main/java/Extension.java
@@ -1,11 +1,16 @@
 import burp.api.montoya.BurpExtension;
 import burp.api.montoya.MontoyaApi;
 
+import javax.swing.*;
+
 public class Extension implements BurpExtension {
     @Override
     public void initialize(MontoyaApi montoyaApi) {
-        montoyaApi.extension().setName("My Extension");
+        montoyaApi.extension().setName("Burp Export History");
 
         // TODO Add your code here
+        JPanel panel = new JPanel();
+        panel.add(new JLabel("abc"));
+        montoyaApi.userInterface().registerSuiteTab("asdasd", panel);
     }
 }
